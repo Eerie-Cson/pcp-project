4c27d46d458559b0af2e4717087b42921627ea85
marshalWilliam <marshalwilliam500@gmail.com>
2025-02-22 16:48:15 +0800

    🌟 chore (builds): scafold build service
    undefined

---
+++ apps/api/src/apps/builds/build.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '../../features/user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('BUILDS_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    UserModule,
+  ],
+  providers: [],
+})
+export class BuildModule {}
