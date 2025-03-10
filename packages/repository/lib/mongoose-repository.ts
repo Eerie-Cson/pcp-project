0b0ff94f5b2507d1885af9ef74e3e8f7b6344e86
marshalWilliam <marshalwilliam500@gmail.com>
2025-03-10 23:57:15 +0800

    🚀 feat (others): add serializeFilter on mongoose repository
    undefined

--- packages/repository/lib/mongoose-repository.ts
+++ packages/repository/lib/mongoose-repository.ts
@@ -1,9 +1,26 @@
 import { Connection, FilterQuery, Model, Schema } from 'mongoose';
 import { Repository } from './repository';
 import { ObjectId } from '@pcp/object-id';
+import * as R from 'ramda';
+
+function serializeFilterField(value: any) {
+  if (value instanceof ObjectId) {
+    return value.toString();
+  }
+
+  return value;
+}
+
+function serializeFilter(filter: any) {
+  if (filter instanceof ObjectId) {
+    return filter.toString();
+  }
+
+  return R.map(serializeFilterField, filter);
+}
 
 export class MongooseRepository<
-  TEntity extends { id: ObjectId } = { id: ObjectId }
+  TEntity extends { id: ObjectId } = { id: ObjectId },
 > implements Repository<TEntity>
 {
   private readonly _model: Model<TEntity>;
@@ -17,43 +34,57 @@ export class MongooseRepository<
   }
 
   public async create(data: TEntity) {
-    await this.model.create(data);
+    await this.model.create(serializeFilter(data));
   }
 
   public async update(
     filter: FilterQuery<TEntity>,
-    data: Partial<Omit<TEntity, 'id'>>
+    data: Partial<Omit<TEntity, 'id'>>,
   ) {
     if (filter instanceof ObjectId) {
-      await this.model.updateOne(filter, {
-        ...data,
-        dateTimeLastUpdated: new Date(),
+      await this.model.updateOne(
+        { id: filter },
+        {
+          ...data,
+          dateTimeLastUpdated: new Date(),
+        },
+      );
+
+      return;
+    }
+
+    await this.model.updateMany(
+      serializeFilter(filter) as FilterQuery<TEntity>,
+      {
+        $set: {
+          ...data,
+          dateTimeLastUpdated: new Date(),
+        },
+      },
+    );
+  }
+
+  public async delete(filter: FilterQuery<TEntity>) {
+    if (filter instanceof ObjectId) {
+      await this.model.deleteOne({
+        id: filter.toString(),
       });
 
       return;
     }
 
-    await this.model.updateMany(filter, {
-      ...data,
-      dateTimeLastUpdated: new Date(),
-    });
-  }
-
-  public async delete(filter: FilterQuery<TEntity>) {
-    if (filter instanceof ObjectId) {
-      await this.model.deleteOne(filter);
-
-      return;
-    }
-
-    await this.model.deleteMany(filter);
+    await this.model.deleteMany(
+      serializeFilter(filter) as FilterQuery<TEntity>,
+    );
   }
 
   public async find(filter?: FilterQuery<TEntity>) {
     if (filter instanceof ObjectId) {
-      return this.model.findOne(filter);
+      return this.model.findOne({ id: filter.toString() });
     }
 
-    return this.model.find(filter || {});
+    return this.model.find(
+      (serializeFilter(filter) as FilterQuery<TEntity>) || {},
+    );
   }
 }
