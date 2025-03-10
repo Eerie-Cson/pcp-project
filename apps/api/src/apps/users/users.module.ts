71ccb45ec972f464d7905823c795a54cb49c89dc
marshalWilliam <marshalwilliam500@gmail.com>
2025-03-10 23:52:11 +0800

    🚀 feat (user): add nestjs graphql
    undefined

--- apps/api/src/apps/users/users.module.ts
+++ apps/api/src/apps/users/users.module.ts
@@ -3,6 +3,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from '../../features/user/user.module';
 import { UsersResolver } from './resolvers/users.resolver';
+import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import {
+  DateTimeResolver,
+  EmailAddressResolver,
+  JSONResolver,
+  URLResolver,
+  TimeZoneResolver,
+  DateResolver,
+} from 'graphql-scalars';
+import path from 'path';
+import { constraintDirectiveTypeDefs } from 'graphql-constraint-directive';
 
 @Module({
   imports: [
@@ -14,7 +26,55 @@ import { UsersResolver } from './resolvers/users.resolver';
       }),
       inject: [ConfigService],
     }),
+    NestGraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: async () => {
+        const options: Omit<ApolloDriverConfig, 'driver'> = {
+          csrfPrevention: false,
+          playground: true,
+          introspection: true,
+          typePaths: [path.resolve(__dirname, './schema/*.gql')],
+          resolvers: {
+            JSON: JSONResolver,
+            Date: DateResolver,
+            DateTime: DateTimeResolver,
+            EmailAddress: EmailAddressResolver,
+            URL: URLResolver,
+            // ObjectId: ObjectIdResolver,
+            // Cursor: CursorResolver,
+            // Decimal: DecimalResolver,
+            Timezone: TimeZoneResolver,
+          },
+          typeDefs: [constraintDirectiveTypeDefs],
+          context: ({ req, res }) => ({
+            // user: req.user,
+            // claims: req.claims,
+            // permissions: req.permissions,
+            // res,
+          }),
+          // transformSchema: async (schema: GraphQLSchema) => {
+          //   let combinedSchemas = schema;
+          //   const schemaTransformers = [
+          //     {
+          //       name: 'assertObjectType',
+          //       fn: assertObjectTypeDirectiveSchemaTransformer,
+          //     },
+          //     {
+          //       name: 'authorizationRequired',
+          //       fn: authorizationRequiredDirectiveSchemaTransformer,
+          //     },
+          //   ];
+          //   schemaTransformers.forEach((s) => {
+          //     combinedSchemas = s.fn(combinedSchemas, s.name);
+          //   });
 
+          //   return combinedSchemas;
+          // },
+        };
+
+        return options;
+      },
+    }),
     UserModule,
   ],
   providers: [UsersResolver],
