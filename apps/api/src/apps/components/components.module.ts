a7ddeadec34ec9930308ee310439a8978c3fee1b
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-03-11 23:44:57 +0800

    🚀 feat (component): update component module
    undefined

--- apps/api/src/apps/components/components.module.ts
+++ apps/api/src/apps/components/components.module.ts
@@ -3,6 +3,11 @@ import { CaseResolver } from './resolvers/case.resolver';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ComponentModule } from '../../features/component/component.module';
+import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DateResolver, ObjectIDResolver } from 'graphql-scalars';
+import path from 'path';
+import { constraintDirectiveTypeDefs } from 'graphql-constraint-directive';
 
 @Module({
   imports: [
@@ -14,7 +19,48 @@ import { ComponentModule } from '../../features/component/component.module';
       }),
       inject: [ConfigService],
     }),
+    NestGraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: async () => {
+        const options: Omit<ApolloDriverConfig, 'driver'> = {
+          csrfPrevention: false,
+          playground: true,
+          introspection: true,
+          typePaths: [path.resolve(__dirname, './schema/*.gql')],
+          resolvers: {
+            ObjectId: ObjectIDResolver,
+            Date: DateResolver,
+          },
+          typeDefs: [constraintDirectiveTypeDefs],
+          context: ({ req, res }) => ({
+            // user: req.user,
+            // claims: req.claims,
+            // permissions: req.permissions,
+            // res,
+          }),
+          // transformSchema: async (schema: GraphQLSchema) => {
+          //   let combinedSchemas = schema;
+          //   const schemaTransformers = [
+          //     {
+          //       name: 'assertObjectType',
+          //       fn: assertObjectTypeDirectiveSchemaTransformer,
+          //     },
+          //     {
+          //       name: 'authorizationRequired',
+          //       fn: authorizationRequiredDirectiveSchemaTransformer,
+          //     },
+          //   ];
+          //   schemaTransformers.forEach((s) => {
+          //     combinedSchemas = s.fn(combinedSchemas, s.name);
+          //   });
 
+          //   return combinedSchemas;
+          // },
+        };
+
+        return options;
+      },
+    }),
     ComponentModule,
   ],
   providers: [CaseResolver],
