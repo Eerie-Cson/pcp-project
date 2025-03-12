a1c86fe73214a3dfc5a1c66ebcf6a792c05f4e0e
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-03-12 13:59:24 +0800

    🔍 test (component): add integration test for update case endpoint
    undefined

---
+++ apps/api/test/component/update-component.test.ts
@@ -0,0 +1,52 @@
+import { setupFixture } from '../component-fixture';
+import { generateComponent } from '../helpers/generate-component';
+import { CaseRepository } from '../../src/features/component/repository/case.repository';
+import { Tokens as ComponentToken } from '../../src/features/component/libs/tokens';
+import { Case, ComponentType } from '@pcp/types';
+import { ObjectTypes } from '@pcp/object-type';
+
+describe('Components', () => {
+  test('Update Case', async () => {
+    const { module, request, teardown } = await setupFixture();
+
+    const caseRepository = module.get<CaseRepository>(
+      ComponentToken.CaseRepository,
+    );
+
+    const caseComponent = await generateComponent(
+      ObjectTypes.CASE,
+      ComponentType.CASE,
+    );
+
+    const updateInput = {
+      name: 'Updated Case',
+    };
+
+    await caseRepository.create(caseComponent);
+
+    const response = await request.post('/graphql').send({
+      query: `
+          mutation(
+            $id: String!
+            $updateCaseInput: UpdateCaseInput!
+          ) {
+            updateCase(id: $id, updateCaseInput: $updateCaseInput)
+          }
+      `,
+      variables: {
+        id: caseComponent.id.toString(),
+        updateCaseInput: updateInput,
+      },
+    });
+
+    const updatedCase = (await caseRepository.find(caseComponent.id)) as Case;
+
+    await teardown();
+    expect(response.status).toBe(200);
+    expect(response.body).not.toHaveProperty('errors');
+    expect(response.body.data.updateCase).toBeTruthy();
+    expect(updatedCase.name).toBe(updateInput.name);
+
+    await teardown();
+  });
+});
