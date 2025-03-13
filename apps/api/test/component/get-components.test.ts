8a75b760bbdb9b885e70e6d14350c5c15b5378e7
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-03-13 10:37:33 +0800

    🔍 test (component): add integration test for query case and query cases
    undefined

---
+++ apps/api/test/component/get-components.test.ts
@@ -0,0 +1,77 @@
+import { setupFixture } from '../component-fixture';
+import { generateComponent } from '../helpers/generate-component';
+import { ComponentType } from '@pcp/types';
+import { CaseRepository } from '../../src/features/component/repository/case.repository';
+import { Tokens as ComponentToken } from '../../src/features/component/libs/tokens';
+import { ObjectTypes } from '@pcp/object-type';
+
+import * as R from 'ramda';
+
+describe('Query.Components', () => {
+  test('Get Cases', async () => {
+    const { module, request, teardown } = await setupFixture();
+
+    const componentRepository = module.get<CaseRepository>(
+      ComponentToken.CaseRepository,
+    );
+
+    const cases = R.times(() =>
+      generateComponent(ObjectTypes.CASE, ComponentType.CASE),
+    )(3);
+
+    await Promise.all(
+      cases.map((component) => componentRepository.create(component)),
+    );
+
+    const getCaseResponse = await request.post('/graphql').send({
+      query: `
+        query($id: String!) {
+          getCase(id: $id) {
+            id
+            name
+            partNumber
+            componentType
+            price
+          }
+        }
+      `,
+      variables: {
+        id: cases[0].id.toString(),
+      },
+    });
+
+    const getCasesResponse = await request.post('/graphql').send({
+      query: `
+        query {
+          getCases {
+            id
+            name
+            partNumber
+            componentType
+            price
+          }
+        }
+      `,
+    });
+
+    await teardown();
+
+    expect(getCaseResponse.status).toEqual(200);
+    expect(getCaseResponse.body).not.toHaveProperty('errors');
+    expect(getCaseResponse.body.data.getCase).toBeTruthy();
+    expect(getCaseResponse.body.data.getCase).toMatchObject({
+      id: cases[0].id.toString(),
+      name: cases[0].name,
+      partNumber: cases[0].partNumber,
+      componentType: ComponentType.CASE,
+      price: cases[0].price,
+    });
+
+    expect(getCasesResponse.status).toEqual(200);
+    expect(getCasesResponse.body.data.getCases).toBeTruthy();
+    expect(getCasesResponse.body).not.toHaveProperty('errors');
+    expect(getCasesResponse.body.data.getCases).toHaveLength(3);
+
+    await teardown();
+  });
+});
