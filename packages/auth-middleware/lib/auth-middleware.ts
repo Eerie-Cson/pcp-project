3ca60372fd20d65a0d9ac3b2cb50f4bd4ddbd7e7
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-03-13 17:57:45 +0800

    🚀 feat (libs): implement auth-middleware
    undefined

---
+++ packages/auth-middleware/lib/auth-middleware.ts
@@ -0,0 +1,27 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async use(
+    req: Request & { claims: any },
+    _res: Response,
+    next: NextFunction,
+  ) {
+    if (!req.headers.authorization) return next();
+
+    console.log(req.headers.authorization);
+    const token = req.headers.authorization.split('Bearer ')[1];
+
+    try {
+      const claims = await this.jwtService.verifyAsync(token);
+      req.claims = claims;
+    } catch (err) {
+      throw new Error();
+    }
+    return next();
+  }
+}
