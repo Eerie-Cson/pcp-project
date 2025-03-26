11fbba9117de20e22fb0feefb19bab1dbc8a28f6
marshalWilliam <marshalwilliam500@gmail.com>
2025-03-26 23:43:09 +0800

    🔥 ref (user): add role to user type and user repository
    undefined

--- packages/types/src/lib/users/user.ts
+++ packages/types/src/lib/users/user.ts
@@ -1,7 +1,9 @@
+import { AccountType } from '../auth/auth';
 import { Node } from '../node';
 export type User = Node & {
   username: string;
   name: string;
+  role: AccountType;
   email: string;
   password: string;
   dateTimeCreated: Date;
