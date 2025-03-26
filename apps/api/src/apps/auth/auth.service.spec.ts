c3921abab264e47a744e108e576ac7dc40660dec
marshalWilliam <marshalwilliam500@gmail.com>
2025-03-26 23:30:57 +0800

    🔍 test (auth): add auth module test
    undefined

---
+++ apps/api/src/apps/auth/auth.service.spec.ts
@@ -0,0 +1,37 @@
+import { Test } from '@nestjs/testing';
+import { getRandomPort } from 'get-port-please';
+import { MongoMemoryReplSet } from 'mongodb-memory-server';
+import { AuthModule } from './auth.module';
+import { ConfigService } from '@nestjs/config';
+
+describe('ComponentModule', () => {
+  test.concurrent('initialize module', async () => {
+    const mongo = await MongoMemoryReplSet.create({
+      replSet: {
+        storageEngine: 'wiredTiger',
+      },
+      instanceOpts: [
+        {
+          launchTimeout: 30000,
+          port: await getRandomPort(),
+        },
+      ],
+    });
+
+    const module = await Test.createTestingModule({
+      imports: [AuthModule],
+    })
+      .overrideProvider(ConfigService)
+      .useValue(
+        new ConfigService({
+          USERS_URI: mongo.getUri('USERS_URI'),
+        })
+      )
+      .compile();
+
+    await expect(module.init()).resolves.toBeTruthy();
+    await expect(module.close()).resolves.toBeUndefined();
+
+    await mongo.stop();
+  });
+});
