4992c785d26c67fb2e06a95e27acd416308110d4
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-04-01 21:52:07 +0800

    🚀 feat (web): create context for authentication
    undefined

---
+++ apps/web/src/context/AuthContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, useContext, useState, ReactNode } from 'react';
+import { Account, AccountType } from '../types/graphql';
+
+type AuthTokens = {
+  accessToken: string;
+  refreshToken: string;
+  session: {
+    id: string;
+    dateTimeCreated: string;
+  };
+};
+
+type AuthContextType = {
+  tokens: AuthTokens | null;
+  login: (data: Account & { password: string }) => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [tokens, setTokens] = useState<AuthTokens | null>(null);
+
+  const login = async (input: Account & { password: string }) => {
+    let data: AuthTokens;
+    try {
+      const response = await fetch('http://localhost:4003/sessions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          role: AccountType.MEMBER,
+          Authorization: `Basic ${window.btoa(`${input.username}:${input.password}`)}`,
+        },
+        body: JSON.stringify(input),
+      });
+
+      data = await response.json();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+    setTokens(data);
+    localStorage.setItem('accessToken', data.accessToken);
+    localStorage.setItem('refreshToken', data.refreshToken);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        tokens,
+        login,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
