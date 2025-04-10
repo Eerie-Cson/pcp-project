e27f14723eda5e39d18a5577ce8403b122759637
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-04-10 19:08:31 +0800

    🐞 fix (component): change heatSpreader to boolean
    undefined

--- apps/api/src/features/component/repository/memory.repository.ts
+++ apps/api/src/features/component/repository/memory.repository.ts
@@ -1,6 +1,6 @@
-import { Connection, Schema } from 'mongoose';
 import { MongooseRepository, Repository } from '@pcp/repository';
 import { Memory } from '@pcp/types';
+import { Connection, Schema } from 'mongoose';
 
 export type MemoryRepository = Repository<Memory>;
 
@@ -51,7 +51,7 @@ export function MemoryRepositoryFactory(connection: Connection) {
       required: true,
     },
     heatSpreader: {
-      type: String,
+      type: Boolean,
       required: true,
     },
     color: {
